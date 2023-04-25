99dab00142bad96b20d75cfcf2b1e72f19cbe522
rukkiecodes <rukkiecodes@gmail.com>
2023-04-25 18:30:47 +0100

    dev

--- app/layouts/StackNavigator.jsx
+++ app/layouts/StackNavigator.jsx
@@ -2,14 +2,18 @@ import React from 'react'
 
 import { createStackNavigator, TransitionPresets } from '@react-navigation/stack'
 
+import Splash from './Splash'
 import Signin from '../screens/auth/Signin'
 import Signup from '../screens/auth/Signup'
 import ForgotPassword from '../screens/auth/ForgotPassword'
 import Navigation from './Navigation'
+import { useSelector } from 'react-redux'
 
 const { Navigator, Screen, Group } = createStackNavigator()
 
 const StackNavigator = () => {
+    const { user, loadingInitial } = useSelector(state => state.user)
+
     return (
         <Navigator
             screenOptions={{
@@ -21,10 +25,17 @@ const StackNavigator = () => {
                 ...TransitionPresets.SlideFromRightIOS
             }}
         >
-            <Screen name="Signin" component={Signin} options={{ gestureEnabled: false }} />
-            <Screen name="Signup" component={Signup} options={{ gestureEnabled: true }} />
-            <Screen name="ForgotPassword" component={ForgotPassword} options={{ gestureEnabled: true }} />
-            <Screen name="Navigation" component={Navigation} options={{ gestureEnabled: false }} />
+            {
+                user ? (
+                    <Screen name="Navigation" component={Navigation} options={{ gestureEnabled: false }} />
+                ) : (
+                    <Group>
+                        <Screen name="Signin" component={Signin} options={{ gestureEnabled: false }} />
+                        <Screen name="Signup" component={Signup} options={{ gestureEnabled: true }} />
+                        <Screen name="ForgotPassword" component={ForgotPassword} options={{ gestureEnabled: true }} />
+                    </Group>
+                )
+            }
         </Navigator>
     )
 }
