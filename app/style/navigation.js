e65160cac40362b1d6ea9d47511912bb9a2ce470
rukkiecodes <rukkiecodes@gmail.com>
2023-04-28 08:24:32 +0100

    remove unwanted style

--- app/style/navigation.js
+++ app/style/navigation.js
@@ -20,24 +20,6 @@ export default StyleSheet.create({
         elevation: 0
     },
 
-    paidImageContainer: {
-        position: 'absolute',
-        zIndex: 1,
-        top: -4,
-        right: -4,
-        width: 15,
-        height: 15,
-        borderRadius: 50,
-        backgroundColor: color.offWhite,
-        justifyContent: 'center',
-        alignItems: 'center'
-    },
-
-    paidImage: {
-        width: 10,
-        height: 10
-    },
-
     avatar: {
         width: 30,
         height: 30,
@@ -83,17 +65,6 @@ export default StyleSheet.create({
         borderRadius: 100
     },
 
-    pointsButton: {
-        flexDirection: 'row',
-        alignItems: 'center'
-    },
-
-    pointsImage: {
-        width: 25,
-        height: 25,
-        marginRight: 5
-    },
-
     usernameButton: {
         flexDirection: 'row',
         marginTop: 15
@@ -103,22 +74,5 @@ export default StyleSheet.create({
         fontSize: 20,
         marginRight: 10,
         color: color.white
-    },
-
-    coin: {
-        color: color.white,
-        fontSize: 16
-    },
-
-    age: {
-        fontSize: 20,
-        color: color.white
-    },
-
-    divider: {
-        backgroundColor: color.white,
-        height: 0.5,
-        width: width / 1.6,
-        marginVertical: 10
     }
 })
