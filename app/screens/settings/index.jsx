3801b257c5cfcaf5c7a452c7bb13336e62ef9d14
rukkiecodes <rukkiecodes@gmail.com>
2023-04-29 19:02:29 +0100

    add head components

--- app/screens/settings/index.jsx
+++ app/screens/settings/index.jsx
@@ -1,9 +1,12 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import style from './style'
+import Header from '../../components/Header'
 
 const Settings = () => {
   return (
-    <View style={{ flex: 1, backgroundColor: 'red' }}>
+    <View style={style.container}>
+      <Header />
       <Text>Settings</Text>
     </View>
   )
