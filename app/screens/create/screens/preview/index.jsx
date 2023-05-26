e436852b52acfd84d8ed76eeef4b40cf0ed8032e
rukkiecodes <rukkiecodes@gmail.com>
2023-05-27 00:50:49 +0100

    add 3 more

--- app/screens/create/screens/preview/index.jsx
+++ app/screens/create/screens/preview/index.jsx
@@ -1,5 +1,5 @@
 import { View, Text, TouchableOpacity } from 'react-native'
-import React from 'react'
+import React, { useLayoutEffect, useState } from 'react'
 
 import { printToFileAsync } from 'expo-print';
 import { shareAsync } from 'expo-sharing';
@@ -13,23 +13,45 @@ import { FontAwesome5 } from '@expo/vector-icons';
 import color from '../../../../style/color';
 import { useNavigation } from '@react-navigation/native';
 import { IV1 } from './templates/IV1';
+import { IV2 } from './templates/IV2';
+import { IV3 } from './templates/IV3';
+import { IV4 } from './templates/IV4';
+import templatesPreview from '../../../../components/fragments/templatesPreview'
+
+import { doc, onSnapshot } from "firebase/firestore"
+import { db } from '../../../../hooks/firebase'
+
+import AsyncStorage from '@react-native-async-storage/async-storage'
 
 const PreviewInvoice = () => {
     const { order, date, dueDate, removeDueDate, invoiceContact, customerName, customerEmail, contact, salesRep, paymentTerms, items, subTotal, vat, total, note } = useSelector(state => state.form)
-    const { profile } = useSelector(state => state.user)
+    const { navigate } = useNavigation()
 
-    const navigation = useNavigation()
+    const [profile, setProfile] = useState(null)
+
+    useLayoutEffect(() => {
+        (async () => {
+            const id = JSON.parse(await AsyncStorage.getItem('recido_user')).user.uid
+
+            const unsub = onSnapshot(doc(db, "users", id), (doc) => {
+                setProfile(doc.data())
+            })
+
+            return unsub
+        })()
+    }, [])
+
 
     let html = ``
 
     switch (profile?.selectedTemplatePreview?.id) {
-        case 1: html = IV1(profile, order, date, invoiceContact, paymentTerms, items, subTotal, vat, total, note) // emove later
+        case 1: html = IV1(profile, order, date, invoiceContact, paymentTerms, items, subTotal, vat, total, note)
             break
-        case 2: html = IV1(profile, order, date, invoiceContact, paymentTerms, items, subTotal, vat, total, note) // emove later
+        case 2: html = IV2(profile, order, date, invoiceContact, paymentTerms, items, subTotal, vat, total, note)
             break
-        case 3: html = IV1(profile, order, date, invoiceContact, paymentTerms, items, subTotal, vat, total, note) // emove later
+        case 3: html = IV3(profile, order, date, invoiceContact, paymentTerms, items, subTotal, vat, total, note)
             break
-        case 4: html = IV1(profile, order, date, invoiceContact, paymentTerms, items, subTotal, vat, total, note)
+        case 4: html = IV4(profile, order, date, invoiceContact, paymentTerms, items, subTotal, vat, total, note)
             break
         default: IV1(profile, order, date, invoiceContact, paymentTerms, items, subTotal, vat, total, note)
     }
@@ -47,7 +69,7 @@ const PreviewInvoice = () => {
         <View style={styles.container}>
             <WebView source={{ html: html }} scalesPageToFit style={{ flex: 1 }} />
             <View style={styles.bottom}>
-                <TouchableOpacity style={styles.shareButton}>
+                <TouchableOpacity onPress={() => navigate('SelectTemplate', { templatesPreview })} style={styles.shareButton}>
                     <Text style={styles.shareButtonText}>Template</Text>
                 </TouchableOpacity>
                 <TouchableOpacity style={styles.goBackBytton}>
