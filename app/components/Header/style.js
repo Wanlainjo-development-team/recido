7a71cc941a448b11ac512175e6b8880a2d87979f
rukkiecodes <rukkiecodes@gmail.com>
2023-05-31 02:45:30 +0100

    can now save invoices

--- app/components/Header/style.js
+++ app/components/Header/style.js
@@ -4,6 +4,7 @@ import color from "../../style/color";
 export default StyleSheet.create({
     container: {
         backgroundColor: color.transparent,
+        marginTop: StatusBar.currentHeight + 20,
         height: 50,
         flexDirection: 'row',
         justifyContent: 'space-between',
