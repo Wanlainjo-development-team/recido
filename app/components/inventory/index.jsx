7ef812f75451b965db527ec15f02dbb746dceef6
rukkiecodes <rukkiecodes@gmail.com>
2023-06-13 08:36:10 +0100

    setup inventory screen

---
+++ app/components/inventory/index.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const InventoryList = () => {
+  return (
+    <View>
+      <Text>InventoryList</Text>
+    </View>
+  )
+}
+
+export default InventoryList
--- app/screens/inventory/index.jsx
+++ app/screens/inventory/index.jsx
@@ -4,12 +4,15 @@ import { useNavigation } from '@react-navigation/native'
 import styles from './styles'
 import { Feather } from '@expo/vector-icons';
 import color from '../../style/color';
+import InventoryList from '../../components/inventory'
 
 const InventoryScreen = () => {
   const { navigate } = useNavigation()
 
   return (
     <View style={styles.container}>
+      <InventoryList />
+
       <TouchableOpacity onPress={() => navigate('AddInventory')} style={styles.floatingButton}>
         <Feather name="plus" size={24} color={color.white} />
       </TouchableOpacity>
