e579c3f7ea2059cd8ad31c1c3a527e976403faae
rukkiecodes <rukkiecodes@gmail.com>
2023-06-15 08:12:07 +0100

    set up setting screen for navigation

--- app/screens/settings/index.jsx
+++ app/screens/settings/index.jsx
@@ -1,244 +1,80 @@
-import {
-  View,
-  Text,
-  TouchableOpacity,
-  TextInput,
-  KeyboardAvoidingView,
-  ScrollView,
-  Platform,
-  Keyboard,
-  TouchableWithoutFeedback,
-  Image,
-  ActivityIndicator,
-  Alert
-} from 'react-native'
-import React, { useState, useLayoutEffect } from 'react'
-import style from './style'
+import { View, Text } from 'react-native'
+import React from 'react'
 import Header from '../../components/Header'
-
-import { Feather } from '@expo/vector-icons';
-import { useDispatch, useSelector } from 'react-redux';
-import * as ImagePicker from 'expo-image-picker'
-import color from '../../style/color';
-import templatesPreview from '../../components/fragments/templatesPreview';
-import { useNavigation } from '@react-navigation/native';
-import AutoHeightImage from 'react-native-auto-height-image'
-import { imageWidth } from '../selectTemplate/style';
-import AsyncStorage from '@react-native-async-storage/async-storage'
-import uuid from 'uuid-random'
-import { setSetup } from '../../features/userSlice'
-
-import { deleteObject, getDownloadURL, getStorage, ref, uploadBytes } from 'firebase/storage'
-import { collection, doc, getDoc, getDocs, onSnapshot, query, updateDoc, where } from 'firebase/firestore'
-import { db } from '../../hooks/firebase';
-import { setSelectedTemplatePreview } from '../../features/useFormSlice';
+import style from './style'
+import { TouchableOpacity } from 'react-native'
+import { ScrollView } from 'react-native'
+import { useNavigation } from '@react-navigation/native'
+import { SimpleLineIcons } from '@expo/vector-icons'
+import color from '../../style/color'
 
 const Settings = () => {
-  const { profile } = useSelector(state => state.user)
-  const { selectedTemplatePreview } = useSelector(state => state.form)
-
-  const navigation = useNavigation()
-
-  const storage = getStorage()
-
-  const dispatch = useDispatch()
-
-  const [image, setImage] = useState(profile?.photoURL == undefined ? null : profile?.photoURL)
-  const [loading, setLoading] = useState(false)
-  const [inputValue, setInputValue] = useState('')
-
-  const [newProfile, setNewProfile] = useState({
-    name: profile?.name,
-    address: profile?.address,
-    email: profile?.email,
-    contact: profile?.contact,
-    salesRep: profile?.salesRep,
-    disclaimer: profile?.disclaimer ? profile?.disclaimer : 'All products are tested and trusted in good working condition. No returns. Products can only be exchanged with the same cash value. All sales are final.'
-  })
-
-  useLayoutEffect(() => {
-    dispatch(setSelectedTemplatePreview(profile?.selectedTemplatePreview))
-  }, [profile])
-
-  const handleContentSizeChange = ({ nativeEvent: { contentSize } }) => {
-    setInputValue(contentSize.height);
-  }
-
-  const pickImage = async () => {
-    let result = await ImagePicker.launchImageLibraryAsync({
-      mediaTypes: ImagePicker.MediaTypeOptions.Images,
-      allowsEditing: true,
-      aspect: [4, 3],
-      quality: 1,
-    });
-
-    if (!result.canceled) {
-      setImage(result.assets[0].uri);
-    }
-  };
-
-  const saveSettings = async () => {
-    const id = JSON.parse(await AsyncStorage.getItem('recido_user')).user.uid
-
-    setLoading(true)
-
-    const blob = await new Promise((resolve, reject) => {
-      const xhr = new XMLHttpRequest()
-      xhr.onload = () => resolve(xhr.response)
-
-      xhr.responseType = 'blob'
-      xhr.open('GET', image, true)
-      xhr.send(null)
-    })
-
-    const link = `logos/${id}/${uuid()}`
-    const photoRef = ref(storage, link)
-
-    const uploadProfile = () => {
-      uploadBytes(photoRef, blob)
-        .then(snapshot => {
-          getDownloadURL(snapshot?.ref)
-            .then(async downloadURL => {
-              await updateDoc(doc(db, 'users', id), {
-                photoURL: downloadURL,
-                photoLink: link,
-                setup: true,
-                selectedTemplatePreview,
-                ...newProfile
-              })
-              setLoading(false)
-              dispatch(setSetup(false))
-              Alert.alert('Bussiness profile successfully uploaded')
-              navigation.goBack()
-            }).catch((e) => setLoading(false))
-        })
-    }
-
-    if (profile.photoURL == undefined) {
-      uploadProfile()
-    } else {
-      const desertRef = ref(storage, profile?.photoLink)
-
-      deleteObject(desertRef).then(() => uploadProfile())
-    }
-  }
+  const { navigate } = useNavigation()
 
   return (
-    <KeyboardAvoidingView style={style.container} behavior={Platform.OS === 'ios' ? 'padding' : 'height'}>
-      <Header />
-      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-        <>
-          <ScrollView showsVerticalScrollIndicator={false}>
-            <View style={style.profileImageView}>
-              <TouchableOpacity onPress={pickImage} style={style.profileImage}>
-                {
-                  !image ?
-                    <Feather name="image" size={24} color="black" /> :
-                    <Image source={{ uri: image }} style={{ width: 200, height: 200 }} />
-                }
-              </TouchableOpacity>
-              <Text>Add Logo</Text>
-            </View>
+    <View style={{ ...style.container, paddingHorizontal: 0 }}>
+      <Header title='Account settings' />
+      <ScrollView style={style.container}>
+        <View style={{ ...style.group, marginTop: 50 }}>
+          <Text style={style.title}>Invoice</Text>
 
-            <View style={style.formView}>
-              <View style={style.inputView}>
-                <Text style={style.inputText}>Please enter a business name</Text>
-                <TextInput
-                  style={style.input}
-                  value={newProfile?.name}
-                  onChangeText={(text) => {
-                    setNewProfile({
-                      ...newProfile,
-                      name: text
-                    })
-                  }}
-                  placeholder='Business name' />
-              </View>
-              <View style={style.inputView}>
-                <Text style={style.inputText}>Please enter a business address</Text>
-                <TextInput
-                  style={style.input}
-                  value={newProfile?.address}
-                  onChangeText={(text) => {
-                    setNewProfile({
-                      ...newProfile,
-                      address: text
-                    })
-                  }}
-                  placeholder='Business address'
-                />
-              </View>
-              <View style={style.inputView}>
-                <Text style={style.inputText}>Your business email</Text>
-                <TextInput style={style.input} editable={false} value={newProfile?.email} placeholder='Business email' />
-              </View>
-              <View style={style.inputView}>
-                <Text style={style.inputText}>Your business contact</Text>
-                <TextInput
-                  style={style.input}
-                  value={newProfile?.contact}
-                  onChangeText={(text) => {
-                    setNewProfile({
-                      ...newProfile,
-                      contact: text
-                    })
-                  }}
-                  placeholder='Business contact'
-                />
-              </View>
-              <View style={style.inputView}>
-                <Text style={style.inputText}>Your business Sales Rep</Text>
-                <TextInput
-                  style={style.input}
-                  value={newProfile?.salesRep}
-                  onChangeText={(text) => {
-                    setNewProfile({
-                      ...newProfile,
-                      salesRep: text
-                    })
-                  }}
-                  placeholder='Sales Rep'
-                />
-              </View>
-              <View style={style.inputView}>
-                <Text style={style.inputText}>Disclaimer</Text>
-                <TextInput
-                  style={{ ...style.input, paddingVertical: 10, height: 100 }}
-                  value={newProfile?.disclaimer}
-                  onChangeText={(text) => {
-                    setNewProfile({
-                      ...newProfile,
-                      disclaimer: text
-                    })
-                  }}
-                  placeholder='Disclaimer'
-                  multiline
-                  onContentSizeChange={handleContentSizeChange}
-                />
-              </View>
-              <TouchableOpacity onPress={() => navigation.navigate('SelectTemplate', { templatesPreview })} style={style.invoicePlaceholderButton}>
-                {
-                  !selectedTemplatePreview ?
-                    <Text style={style.invoicePlaceholderButtonText}>Select invoice template</Text> :
-                    <AutoHeightImage
-                      width={imageWidth}
-                      source={{ uri: selectedTemplatePreview?.preview }}
-                    />
-                }
-              </TouchableOpacity>
-            </View>
-          </ScrollView>
-
-          <TouchableOpacity onPress={saveSettings} style={style.saveButton}>
-            {
-              loading ?
-                <ActivityIndicator size='small' color={color.white} /> :
-                <Text style={style.saveButtonText}>Save Settings</Text>
-            }
+          <TouchableOpacity style={style.actionButton}>
+            <Text style={style.actionButtonTitle}>Bussiness details</Text>
+            <Text style={style.actionButtonSubTitle}>Logo, name, contact information</Text>
           </TouchableOpacity>
-        </>
-      </TouchableWithoutFeedback>
-    </KeyboardAvoidingView>
+
+          <TouchableOpacity style={style.actionButton}>
+            <Text style={style.actionButtonTitle}>Template</Text>
+            <Text style={style.actionButtonSubTitle}>Select your prefared invoice design</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style={style.actionButton}>
+            <Text style={style.actionButtonTitle}>Tax</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style={style.actionButton}>
+            <Text style={style.actionButtonTitle}>Default notes</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style={style.actionButton}>
+            <Text style={style.actionButtonTitle}>Invoice number</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style={style.actionButton}>
+            <Text style={style.actionButtonTitle}>Export as spreadsheet</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style={style.actionButton}>
+            <Text style={style.actionButtonTitle}>Customize</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style={style.actionButton}>
+            <Text style={style.actionButtonTitle}>Default Email message</Text>
+          </TouchableOpacity>
+        </View>
+
+        <View style={{ ...style.group, marginTop: 50 }}>
+          <Text style={style.title}>Information</Text>
+
+          <TouchableOpacity style={style.actionButton}>
+            <Text style={style.actionButtonTitle}>About</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style={style.actionButton}>
+            <Text style={style.actionButtonTitle}>Terms of use</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style={style.actionButton}>
+            <Text style={style.actionButtonTitle}>Privacy policy</Text>
+          </TouchableOpacity>
+        </View>
+
+        <TouchableOpacity style={style.logoutButton}>
+          <SimpleLineIcons name="logout" size={20} color={color.red} style={{ marginRight: 15 }} />
+          <Text style={style.logoutButtonText}>Log out</Text>
+        </TouchableOpacity>
+      </ScrollView>
+    </View>
   )
 }
 
--- app/screens/settings/style.js
+++ app/screens/settings/style.js
@@ -4,70 +4,49 @@ import color from "../../style/color";
 export default StyleSheet.create({
     container: {
         flex: 1,
-        paddingTop: StatusBar.currentHeight + 20,
         backgroundColor: color.mainBackground,
+        paddingHorizontal: 10
     },
 
-    profileImageView: {
-        justifyContent: 'center',
-        alignItems: 'center',
-        marginVertical: 20
+    group: {},
+
+    title: {
+        fontSize: 30,
+        fontWeight: '900',
+        opacity: .40,
+        marginBottom: 20
     },
 
-    profileImage: {
-        backgroundColor: color.white,
-        borderRadius: 100,
-        height: 100,
-        width: 100,
-        justifyContent: 'center',
+    actionButton: {
+        paddingVertical: 10,
+        borderBottomWidth: 1,
+        borderBottomColor: `${color.accent}40`,
+        marginBottom: 10,
+        justifyContent: 'center'
+    },
+
+    actionButtonTitle: {
+        fontSize: 18
+    },
+
+    actionButtonSubTitle: {
+        opacity: .50
+    },
+
+    logoutButton: {
+        marginTop: 20,
+        backgroundColor: `${color.red}40`,
+        flexDirection: 'row',
+        justifyContent: 'flex-start',
         alignItems: 'center',
-        overflow: 'hidden',
+        height: 45,
+        borderRadius: 12,
+        paddingHorizontal: 10,
         marginBottom: 10
     },
 
-    formView: {
-        paddingHorizontal: 20,
-        marginVertical: 20
-    },
-
-    inputView: {
-        marginBottom: 30
-    },
-
-    inputText: {
-        fontSize: 13
-    },
-
-    input: {
-        backgroundColor: `${color.accent}20`,
-        height: 45,
-        marginTop: 10,
-        borderRadius: 12,
-        paddingHorizontal: 10,
+    logoutButtonText: {
+        color: color.red,
         fontWeight: '600'
-    },
-
-    invoicePlaceholderButton: {
-        width: '100%',
-        minHeight: 250,
-        backgroundColor: `${color.accent}20`,
-        borderRadius: 12,
-        justifyContent: 'center',
-        alignItems: 'center'
-    },
-
-    invoicePlaceholderButtonText: {},
-
-    saveButton: {
-        backgroundColor: color.accent,
-        margin: 10,
-        height: 45,
-        borderRadius: 12,
-        justifyContent: 'center',
-        alignItems: 'center'
-    },
-
-    saveButtonText: {
-        color: color.white
     }
 })
