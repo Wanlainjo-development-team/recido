67bd8236146083bb4af83b058f0d038e0d0738d4
rukkiecodes <rukkiecodes@gmail.com>
2023-06-17 23:18:50 +0100

    display templates

---
+++ app/screens/settings/screens/template/screens/Style.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Style = () => {
+  return (
+    <View>
+      <Text>Style</Text>
+    </View>
+  )
+}
+
+export default Style
---
+++ app/screens/settings/screens/template/screens/Templates.jsx
@@ -0,0 +1,47 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import templatesPreview from '../../../../../components/fragments/templatesPreview'
+import styles from './templateStyle'
+import { ScrollView } from 'react-native'
+import { TouchableOpacity } from 'react-native'
+import AutoHeightImage from 'react-native-auto-height-image'
+import { Dimensions } from 'react-native'
+import { useSelector } from 'react-redux'
+import color from '../../../../../style/color'
+import { doc, updateDoc } from 'firebase/firestore'
+import { db } from '../../../../../hooks/firebase'
+import AsyncStorage from '@react-native-async-storage/async-storage'
+
+
+const { width } = Dimensions.get('window')
+
+const Template = () => {
+    const { profile } = useSelector(state => state.user)
+
+    const updateChosenTemplate = async item => {
+        const id = JSON.parse(await AsyncStorage.getItem('recido_user')).user.uid
+
+        await updateDoc(doc(db, 'users', id), { selectedTemplatePreview: item })
+    }
+
+    return (
+        <View style={styles.container}>
+            <ScrollView showsVerticalScrollIndicator={false} style={{ flex: 1 }}>
+                <View style={styles.previewGrid}>
+                    {
+                        templatesPreview.map((item, index) => (
+                            <TouchableOpacity key={index} onPress={() => updateChosenTemplate(item)} style={{ ...styles.imageButton, borderColor: item.invoice == profile?.selectedTemplatePreview?.invoice ? color.accent : color.transparent }}>
+                                <AutoHeightImage
+                                    width={(width / 2) - 15}
+                                    source={{ uri: item?.preview }}
+                                />
+                            </TouchableOpacity>
+                        ))
+                    }
+                </View>
+            </ScrollView>
+        </View>
+    )
+}
+
+export default Template
